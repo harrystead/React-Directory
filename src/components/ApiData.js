d16a50b0864b4ad5a43f5c2200149f3144d08894
Harry Stead <harrystead9@gmail.com>
2021-03-18 13:09:36 +0000

    initial setup

---
+++ src/components/ApiData.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ApiData () {
+
+    return (
+        <div>
+            <h2>
+                hello
+            </h2>
+        </div>
+    )
+}
